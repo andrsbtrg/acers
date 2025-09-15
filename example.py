d91922b97a2e7159638db8485fa4f23513965106
Andres <andrsbtrg@gmail.com>
2025-09-15 10:58:33 +0200

    chore: add example files

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 from acers import clash_detection
 
 
-with open("set_a.txt", "r") as file:
+with open("./examples/set_a.txt", "r") as file:
     set_a = file.read()
 
-with open("set_b.txt", "r") as file:
+with open("./examples/set_b.txt", "r") as file:
     set_b = file.read()
 
 results = clash_detection(set_a, set_b, 0.0)
